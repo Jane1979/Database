d6827ae77535a832d9ee30739f9e45d465849671
Mihai George Forlafu <mihaigeorgeforlafu@gmail.com>
2019-12-17 12:36:34 +0100

    Frontend

--- backend2/server.js
+++ backend2/server.js
@@ -1,18 +1,44 @@
-var express = require('express');
-var app = express(),
-  port = process.env.PORT || 3010,
-  bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const sqlInstance = require('mssql/msnodesqlv8');
+const url = require('url');
 
-var cors = require('cors');
-app.use(cors());
+const port = 3000;
+
+app.use(express.static('public'));
+
+var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// configuration for database
+var configDB = {
+    driver: "msnodesqlv8",
+    server: 'localhost',
+    database: 'Product',
+    options: {
+        trustedConnection: true,
+        useUTC: true
+      }
+};
 
-var routes = require('./api/routes/gamesRoutes');
-routes(app);
 
-
-var server = app.listen(port, function () {
-    console.log('Server is running on port '+ port);
+// This endpoint fetches all products from the database, ordering them by price ascending
+app.get('/products', function (req, res) {
+    sqlInstance.connect(configDB, function (err) {
+        if (err) console.log(err);
+        // create Request object
+        var request = new sqlInstance.Request();
+        // query to the database and get the products
+        request.query('SELECT * FROM Product', function (err, products) {
+            if (err) console.log(err);
+            // send records as a response
+            // console.log(products);
+            res.status(200).json({
+                products: products
+            });
+        }); 
+    });
 });
+
+app.listen(process.env.PORT || port, () => console.log(`Listening on port ${port}!`))
